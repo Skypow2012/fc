3a215781112c0b4143859d48a47ec6f13c012031
maxmon <541182180@qq.com>
2019-09-02 04:05:35 +0800

    1、新增伪联机功能（需要同一屏幕，共享按键）； 2、新增存档功能（存储nes）；

---
+++ bg/ws.js
@@ -0,0 +1,26 @@
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+app.get('/', function(req, res){
+    res.send('<h1>Welcome Realtime Server</h1>');
+});
+
+let sockets = [];
+
+io.on('connection', function(socket){
+    console.log('a user connected');
+    sockets.push(socket);
+
+    socket.on("disconnect", function() {
+        console.log("a user go out");
+    });
+
+    socket.on("message", function(obj) {
+        io.emit("message", obj);
+    });
+});
+
+http.listen(3000, function(){
+    console.log('listening on *:3000');
+});
